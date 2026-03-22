c5a086b90d1de1b66e7068e2c793adb559ebc68b
YehyaCh002 <chehida.yehya@univ-guelma.dz>
2026-03-22 19:13:48 +0100

    we fix the  CI

--- src/migrations/1775139000000-AddDeliveryTypeAndStoreFlag.ts
+++ src/migrations/1775139000000-AddDeliveryTypeAndStoreFlag.ts
@@ -4,13 +4,13 @@ export class AddDeliveryTypeAndStoreFlag1775139000000 implements MigrationInterf
     name = 'AddDeliveryTypeAndStoreFlag1775139000000'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "orders" ADD "deliveryType" varchar(50) DEFAULT 'Domicile'`);
-        await queryRunner.query(`ALTER TABLE "orders" ADD "soldFromStore" boolean DEFAULT false`);
+        await queryRunner.query(`ALTER TABLE "orders" ADD COLUMN IF NOT EXISTS "deliveryType" varchar(50) DEFAULT 'Domicile'`);
+        await queryRunner.query(`ALTER TABLE "orders" ADD COLUMN IF NOT EXISTS "soldFromStore" boolean DEFAULT false`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "orders" DROP COLUMN "soldFromStore"`);
-        await queryRunner.query(`ALTER TABLE "orders" DROP COLUMN "deliveryType"`);
+        await queryRunner.query(`ALTER TABLE "orders" DROP COLUMN IF EXISTS "soldFromStore"`);
+        await queryRunner.query(`ALTER TABLE "orders" DROP COLUMN IF EXISTS "deliveryType"`);
     }
 
 }
